cf58c2c442cad0df29be7843a7855de2b60689ad
sagnikmitra <sagnikmitra123@gmail.com>
2021-07-04 18:13:05 +0530

    update

--- sagnik_mitra_2027_assignment_4_chat_udp_server1.c
+++ sagnik_mitra_2027_assignment_4_chat_udp_server1.c
@@ -11,81 +11,72 @@
 
 int server_fd;
 
-void *receive(void * arg)
+void *receive(void *arg)
 {
-	
+
 	while (1)
 	{
-		
-		
-		
-		char buffer[1024] = {0};		
+
+		char buffer[1024] = {0};
 		struct sockaddr_in address2;
-		
-		int length=sizeof(address2);
-		
-		recvfrom(server_fd, (char *)buffer, 1024, 0,(struct sockaddr *) &address2, &length);
-		
-		printf("\nServer 2: %s\n", buffer); 
+
+		int length = sizeof(address2);
+
+		recvfrom(server_fd, (char *)buffer, 1024, 0, (struct sockaddr *)&address2, &length);
+
+		printf("\nServer 2: %s\n", buffer);
 	}
 }
 
-void *sendm(void * arg)
+void *sendm(void *arg)
 {
-	
-	while(1)
+
+	while (1)
 	{
 		int new_socket, valread;
 		struct sockaddr_in address2;
 
 		char buffer[1024] = {0};
-		
+
 		char hello[1024];
-		
+
 		printf("Enter message to send to server 1: ");
-		scanf("%s",hello);
-		
-		
-		
+		scanf("%s", hello);
+
 		address2.sin_family = AF_INET;
 		address2.sin_addr.s_addr = inet_addr("127.0.0.1");
-		address2.sin_port = htons( 1235 );
-		
-		   
-		sendto(server_fd, (const char *)hello, strlen(hello), 0, (const struct sockaddr *) &address2,sizeof(address2));
+		address2.sin_port = htons(1235);
+
+		sendto(server_fd, (const char *)hello, strlen(hello), 0, (const struct sockaddr *)&address2, sizeof(address2));
 		printf("message sent.\n");
 	}
 }
 
 int main()
 {
-	
+
 	pthread_t pt[2];
 	int new_socket, valread;
-    struct sockaddr_in address1,address2;
-    int addrlen = sizeof(address1);
-    char buffer[1024] = {0};
-    char *hello = "Hello client!";
-    
-    server_fd = socket(AF_INET, SOCK_DGRAM, 0);
-    
-   
-    address1.sin_family = AF_INET;
-    address1.sin_addr.s_addr = inet_addr("127.0.0.1");
-    address1.sin_port = htons( 1234 );
-    
-    
-    bind(server_fd, (struct sockaddr *)&address1, sizeof(address1));
+	struct sockaddr_in address1, address2;
+	int addrlen = sizeof(address1);
+	char buffer[1024] = {0};
+	char *hello = "Hello client!";
 
-	pthread_create(&pt[0],NULL,receive,NULL);    
-	pthread_create(&pt[1],NULL,sendm,NULL);
-	
-	pthread_join(pt[0],NULL);
-	pthread_join(pt[1],NULL);
-	
+	server_fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	address1.sin_family = AF_INET;
+	address1.sin_addr.s_addr = inet_addr("127.0.0.1");
+	address1.sin_port = htons(1234);
+
+	bind(server_fd, (struct sockaddr *)&address1, sizeof(address1));
+
+	pthread_create(&pt[0], NULL, receive, NULL);
+	pthread_create(&pt[1], NULL, sendm, NULL);
+
+	pthread_join(pt[0], NULL);
+	pthread_join(pt[1], NULL);
+
 	pthread_exit(NULL);
-	
-	
-	
+
 	return 0;
 }
--- sagnik_mitra_2027_assignment_6_multicast_group_A.c
+++ sagnik_mitra_2027_assignment_6_multicast_group_A.c
@@ -1,28 +1,27 @@
-/*		PRITI JANA
-		GROUP STRUCTURE USING MULTICASTING
-		PROGRAM A
-		IT RECEIVES MESSAGES FROM GROUP 1, GROUP 2, GROUP 3 AND GROUP 4*/
-//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+/*			
+Multicasting Group A
+Sagnik Mitra | 1811100002027
+Computer Networks Lab Assignment
+*/
 
-
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<netdb.h>
-#include<netinet/in.h>
-#include<unistd.h>
-#include<sys/socket.h>
-#include<sys/types.h>
-#include<arpa/inet.h>
-#include<pthread.h>
-#include<errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <errno.h>
 extern int errno;
 
-int sid1, sid2, sid3, sid4; 
+int sid1, sid2, sid3, sid4;
 
-void *receive_g1(void * arg)
+void *receive_g1(void *arg)
 {
-	while(1)
+	while (1)
 	{
 		char buffer[1024] = {0};
 		struct sockaddr_in address;
@@ -32,9 +31,9 @@ void *receive_g1(void * arg)
 	}
 }
 
-void *receive_g2(void * arg)
+void *receive_g2(void *arg)
 {
-	while(1)
+	while (1)
 	{
 		char buffer[1024] = {0};
 		struct sockaddr_in address;
@@ -44,9 +43,9 @@ void *receive_g2(void * arg)
 	}
 }
 
-void *receive_g3(void * arg)
+void *receive_g3(void *arg)
 {
-	while(1)
+	while (1)
 	{
 		char buffer[1024] = {0};
 		struct sockaddr_in address;
@@ -56,9 +55,9 @@ void *receive_g3(void * arg)
 	}
 }
 
-void *receive_g4(void * arg)
+void *receive_g4(void *arg)
 {
-	while(1)
+	while (1)
 	{
 		char buffer[1024] = {0};
 		struct sockaddr_in address;
@@ -68,9 +67,9 @@ void *receive_g4(void * arg)
 	}
 }
 
-void *sendT(void * arg)
+void *sendT(void *arg)
 {
-	while(1)
+	while (1)
 	{
 		int group;
 		struct sockaddr_in address;
@@ -80,41 +79,37 @@ void *sendT(void * arg)
 		scanf("%d", &group);
 		printf("\nEnter Message to group %d: ", group);
 		scanf("%s", hello);
-		
-		
-		
+
 		address.sin_family = AF_INET;
-		
-		if(group == 1)
+
+		if (group == 1)
 		{
 			address.sin_addr.s_addr = inet_addr("230.1.1.1");
 			address.sin_port = htons(1111);
-			sendto(sid1, (const char *)hello, strlen(hello)+1, 0, (const struct sockaddr *)&address, sizeof(address));
+			sendto(sid1, (const char *)hello, strlen(hello) + 1, 0, (const struct sockaddr *)&address, sizeof(address));
 			printf("\nMessage Sent to group 1");
 		}
-		else if(group == 2)
+		else if (group == 2)
 		{
 			address.sin_addr.s_addr = inet_addr("230.1.1.2");
 			address.sin_port = htons(2222);
-			sendto(sid2, (const char *)hello, strlen(hello)+1, 0, (const struct sockaddr *)&address, sizeof(address));
+			sendto(sid2, (const char *)hello, strlen(hello) + 1, 0, (const struct sockaddr *)&address, sizeof(address));
 			printf("\nMessage Sent to group 2");
 		}
-		else if(group == 3)
+		else if (group == 3)
 		{
 			address.sin_addr.s_addr = inet_addr("230.1.1.3");
 			address.sin_port = htons(3333);
-			sendto(sid3, (const char *)hello, strlen(hello)+1, 0, (const struct sockaddr *)&address, sizeof(address));
+			sendto(sid3, (const char *)hello, strlen(hello) + 1, 0, (const struct sockaddr *)&address, sizeof(address));
 			printf("\nMessage Sent to group 3");
-			
 		}
-		else if(group == 4)
+		else if (group == 4)
 		{
 			address.sin_addr.s_addr = inet_addr("230.1.1.4");
 			address.sin_port = htons(4444);
-			sendto(sid4, (const char *)hello, strlen(hello)+1, 0, (const struct sockaddr *)&address, sizeof(address));
+			sendto(sid4, (const char *)hello, strlen(hello) + 1, 0, (const struct sockaddr *)&address, sizeof(address));
 			printf("\nMessage Sent to group 4");
 		}
-		
 	}
 }
 
@@ -125,114 +120,78 @@ int main()
 	char buffer[1024] = {0};
 	char *hello = "Hello Group!!";
 	pthread_t pt[5];
-	
+
 	sid1 = socket(AF_INET, SOCK_DGRAM, 0);
 	sid2 = socket(AF_INET, SOCK_DGRAM, 0);
 	sid3 = socket(AF_INET, SOCK_DGRAM, 0);
 	sid4 = socket(AF_INET, SOCK_DGRAM, 0);
-	
+
 	address1.sin_family = AF_INET;
 	address1.sin_addr.s_addr = INADDR_ANY;
 	address1.sin_port = htons(1111);
-	
+
 	address2.sin_family = AF_INET;
 	address2.sin_addr.s_addr = INADDR_ANY;
 	address2.sin_port = htons(2222);
-	
+
 	address3.sin_family = AF_INET;
 	address3.sin_addr.s_addr = INADDR_ANY;
 	address3.sin_port = htons(3333);
-	
+
 	address4.sin_family = AF_INET;
 	address4.sin_addr.s_addr = INADDR_ANY;
 	address4.sin_port = htons(4444);
-	
+
 	setsockopt(sid1, SOL_SOCKET, SO_REUSEADDR, (void *)&val, sizeof(val));
 	perror("setsockopt 1");
 	bind(sid1, (struct sockaddr *)&address1, sizeof(address1));
 	perror("bind");
-	
+
 	setsockopt(sid2, SOL_SOCKET, SO_REUSEADDR, (void *)&val, sizeof(val));
 	perror("setsockopt 2");
 	bind(sid2, (struct sockaddr *)&address2, sizeof(address2));
 	perror("bind");
-	
+
 	setsockopt(sid3, SOL_SOCKET, SO_REUSEADDR, (void *)&val, sizeof(val));
 	perror("setsockopt 3");
 	bind(sid3, (struct sockaddr *)&address3, sizeof(address3));
 	perror("bind");
-	
+
 	setsockopt(sid4, SOL_SOCKET, SO_REUSEADDR, (void *)&val, sizeof(val));
 	perror("setsockopt 4");
 	bind(sid4, (struct sockaddr *)&address4, sizeof(address4));
 	perror("bind");
-	
+
 	struct ip_mreq m1, m2, m3, m4;
 	m1.imr_multiaddr.s_addr = inet_addr("230.1.1.1");
 	m1.imr_interface.s_addr = INADDR_ANY;
 	setsockopt(sid1, IPPROTO_IP, IP_ADD_MEMBERSHIP, &m1, sizeof(m1));
-	
+
 	m2.imr_multiaddr.s_addr = inet_addr("230.1.1.2");
 	m2.imr_interface.s_addr = INADDR_ANY;
 	setsockopt(sid2, IPPROTO_IP, IP_ADD_MEMBERSHIP, &m2, sizeof(m2));
-	
+
 	m3.imr_multiaddr.s_addr = inet_addr("230.1.1.3");
 	m3.imr_interface.s_addr = INADDR_ANY;
 	setsockopt(sid3, IPPROTO_IP, IP_ADD_MEMBERSHIP, &m3, sizeof(m3));
-	
+
 	m4.imr_multiaddr.s_addr = inet_addr("230.1.1.4");
 	m4.imr_interface.s_addr = INADDR_ANY;
 	setsockopt(sid4, IPPROTO_IP, IP_ADD_MEMBERSHIP, &m4, sizeof(m4));
-	
+
 	perror("setsockopt 5");
-	
+
 	pthread_create(&pt[0], NULL, sendT, NULL);
 	pthread_create(&pt[1], NULL, receive_g1, NULL);
 	pthread_create(&pt[2], NULL, receive_g2, NULL);
 	pthread_create(&pt[3], NULL, receive_g3, NULL);
 	pthread_create(&pt[4], NULL, receive_g4, NULL);
-	
+
 	pthread_join(pt[0], NULL);
 	pthread_join(pt[1], NULL);
 	pthread_join(pt[2], NULL);
 	pthread_join(pt[3], NULL);
 	pthread_join(pt[4], NULL);
-	
+
 	pthread_exit(NULL);
 }
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
